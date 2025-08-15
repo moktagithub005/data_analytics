c37de02bd4d86a9bd9ac5fc00b923444772a195b
ajay mokta <ajaymokta2002@ajays-MacBook-Air.local>
2025-08-15 18:16:28 +0530

    add all code files

---
+++ lec3_where_clause.sql
@@ -0,0 +1,47 @@
+#where clause
+
+#Let's take a look at how this looks
+SELECT *
+FROM employee_salary
+WHERE salary > 50000;
+
+SELECT *
+FROM employee_salary
+WHERE salary >= 50000;
+
+SELECT *
+FROM employee_demographics
+WHERE gender = 'Female';
+
+#We can also return rows that do have not "Scranton"
+SELECT *
+FROM employee_demographics
+WHERE gender != 'Female';
+
+#We can use WHERE clause with date value also
+SELECT *
+FROM employee_demographics
+WHERE birth_date > '1985-01-01';
+
+-- Here '1990-01-01' is the default data formate in MySQL.
+-- There are other date formats as well that we will talk about in a later lesson.
+
+# LIKE STATEMENT
+
+-- two special characters a % and a _
+
+-- % means anything
+SELECT *
+FROM employee_demographics
+WHERE first_name LIKE 'a%';
+
+-- _ means a specific value
+SELECT *
+FROM employee_demographics
+WHERE first_name LIKE 'a__';
+
+SELECT *
+FROM employee_demographics
+WHERE first_name LIKE 'a___%';
+
+
