1b461e6920faf2bf06e81d115208439760be24bf
moktagithub005 <moktaajay@gmail.om>
2025-09-02 16:32:06 +0530

    add all new files

---
+++ DA_MOKTA/INSERT.sql
@@ -0,0 +1,10 @@
+-- DML(data manipulation language) : INSERT
+
+-- lets INSERT into customers table 
+
+INSERT INTO customers(id,first_name,country,score)
+VALUES
+	(6,'MOKTA','india',NULL),
+	(7,'muskan',NULL,100)
+    
+-- columns and values must match 
